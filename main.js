73be2275a6ada5e83727e134850b2273263c2d13
Axel Garcia <axel.garcia94@gmail.com>
2018-10-31 12:17:42 +0100

    main and game transitions

--- main.js
+++ main.js
@@ -1,7 +1,89 @@
 'use strict'
 
+function buildDOM(html) {
+  var div = document.createElement('div');
+  div.innerHTML = html;
+  return div.children[0];
+}
+
 function main() {
-  console.log('main')
+
+  var splashScreen;
+  var gameScreen;
+  var gameOverScreen;
+
+  var startButton;
+  var restartButton;
+  
+  function buildSplash() {
+    splashScreen = buildDOM(`
+      <main>
+        <h1>Eternal Enemies</h1>
+        <button>Start</button>
+      </main>
+    `)
+
+    document.body.prepend(splashScreen);
+
+    startButton = document.querySelector('button');
+
+    startButton.addEventListener('click', destroySplash);
+  }
+
+  function destroySplash() {
+    splashScreen.remove();
+    startButton.removeEventListener('click', destroySplash);
+
+    buildGameScreen();
+  }
+
+  function buildGameScreen() {
+    gameScreen = buildDOM(`
+      <main>
+        <p class="lives">3</p>     
+        <canvas></canvas>   
+      </main>
+    `);
+
+    document.body.prepend(gameScreen);
+
+    setTimeout(function() {
+      destroyGameScreen()
+    }, 3000)
+
+  }
+
+  function destroyGameScreen() {
+    gameScreen.remove();
+    buildGameOverScreen();
+  }
+
+  function buildGameOverScreen() {
+    gameOverScreen = buildDOM(`
+      <main>
+        <h1>Game Over</h1>
+        <button>Restart</button>
+      </main>  
+    `);
+
+    document.body.prepend(gameOverScreen);
+
+    restartButton = document.querySelector('button');
+
+    restartButton.addEventListener('click', destroyGameOverScreen)
+
+  }
+
+  function destroyGameOverScreen() {
+    gameOverScreen.remove();
+
+    restartButton.removeEventListener('click', destroyGameOverScreen)
+
+    buildGameScreen();
+  }
+
+  buildSplash();
+
 }
 
 window.addEventListener('load', main);
